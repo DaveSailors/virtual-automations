9ff790e4244307a0b32b6c07017fa3cffe7d95f1
Dave Sailors <davesailors@yahoo.com>
2017-09-06 00:11:48 -0700

    initial terraform checkin

---
+++ terraform/deployami.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "us-west-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-de6a5dbe"
+  instance_type = "t2.micro"
+}
+
