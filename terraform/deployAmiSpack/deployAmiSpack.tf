3cb2af9038bd257928e2fc1546e1c87faaff57e0
Dave Sailors <davesailors@yahoo.com>
2017-09-27 21:21:58 -0700

    start with basic deployami

---
+++ terraform/deployAmiSpack/deployAmiSpack.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "us-west-1"
+}
+
+resource "aws_instance" "example" {
+  ami = "${var.ami_id}"
+  instance_type = "t2.micro"
+}
+
