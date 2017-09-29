a92fabea21f13f547275fa72ff26bb5f9a657ac1
Dave Sailors <davesailors@yahoo.com>
2017-09-29 02:15:22 -0700

    more

--- terraform/deployAmiSpack/userdata.sh
+++ terraform/deployAmiSpack/userdata.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-date >/tmp/userdata.log
-echo ...userdata.sh startingxi... >>/tmp/userdata.log
-sudo yum -y install git.x86_64 >>/tmp/userdata.log
-git help >>/tmp/userdata.log
+date >/tmp/userdata.log 2>&1
+echo ...userdata.sh starting... >>/tmp/userdata.log 2>&1
+sudo yum -y install git.x86_64 >>/tmp/userdata.log 2>&1
+git help >>/tmp/userdata.log 2>&1
+mkdir /opt/spack >>/tmp/userdata.log 2>&1
+cd /opt/spack >>/tmp/userdata.log 2>&1
+git clone https://github.com/LLNL/spack.git >>/tmp/userdata.log 2>&1
 
