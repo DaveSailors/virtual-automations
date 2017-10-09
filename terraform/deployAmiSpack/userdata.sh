e187d0c4fd472f62a221ed890f0b76f6f3408087
Dave Sailors <davesailors@yahoo.com>
2017-10-09 15:10:05 -0700

    updates

--- terraform/deployAmiSpack/userdata.sh
+++ terraform/deployAmiSpack/userdata.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
+echo "***********************************" >>/tmp/userdata.log 2>&1
 date >/tmp/userdata.log 2>&1
+echo "***********************************" >>/tmp/userdata.log 2>&1
 echo ...userdata.sh starting... >>/tmp/userdata.log 2>&1
+echo "***********************************" >>/tmp/userdata.log 2>&1
+sudo yum -y update >>/tmp/userdata.log 2>&1
+echo "***********************************" >>/tmp/userdata.log 2>&1
+echo "***********************************" >>/tmp/userdata.log 2>&1
 sudo yum -y install git.x86_64 >>/tmp/userdata.log 2>&1
+echo "***********************************" >>/tmp/userdata.log 2>&1
+echo "***********************************" >>/tmp/userdata.log 2>&1
 git help >>/tmp/userdata.log 2>&1
-mkdir /opt/spack >>/tmp/userdata.log 2>&1
+echo "***********************************" >>/tmp/userdata.log 2>&1
+sudo mkdir /opt/spack >>/tmp/userdata.log 2>&1
+echo "***********************************" >>/tmp/userdata.log 2>&1
 cd /opt/spack >>/tmp/userdata.log 2>&1
-git clone https://github.com/LLNL/spack.git >>/tmp/userdata.log 2>&1
+echo "***********************************" >>/tmp/userdata.log 2>&1
+sudo git clone https://github.com/LLNL/spack.git >>/tmp/userdata.log 2>&1
+echo "***********************************" >>/tmp/userdata.log 2>&1
+sudo /opt/spack/spack/bin/spack
+echo "***********************************" >>/tmp/userdata.log 2>&1
 cat /tmp/userdata.log
+echo "***********************************" >>/tmp/userdata.log 2>&1
+date >>/tmp/userdata.log 2>&1
+echo ...userdata.sh endinging... >>/tmp/userdata.log 2>&1
+echo "***********************************" >>/tmp/userdata.log 2>&1
 
 
