a855b6ad6e13a6d25bac6d269b038dcb3e0eb7a4
Dave Sailors <davesailors@yahoo.com>
2017-10-09 18:25:48 -0700

    updates

--- terraform/deployAmiSpack/userdata.sh
+++ terraform/deployAmiSpack/userdata.sh
@@ -10,15 +10,22 @@ echo "***********************************" >>/tmp/userdata.log 2>&1
 sudo yum -y install git.x86_64 >>/tmp/userdata.log 2>&1
 echo "***********************************" >>/tmp/userdata.log 2>&1
 echo "***********************************" >>/tmp/userdata.log 2>&1
+sudo yum -y install gcc.noarch >>/tmp/userdata.log 2>&1
+echo "***********************************" >>/tmp/userdata.log 2>&1
+echo "***********************************" >>/tmp/userdata.log 2>&1
 git help >>/tmp/userdata.log 2>&1
 echo "***********************************" >>/tmp/userdata.log 2>&1
 sudo mkdir /opt/spack >>/tmp/userdata.log 2>&1
 echo "***********************************" >>/tmp/userdata.log 2>&1
 cd /opt/spack >>/tmp/userdata.log 2>&1
 echo "***********************************" >>/tmp/userdata.log 2>&1
+echo "***********************************" >>/tmp/userdata.log 2>&1
 sudo git clone https://github.com/LLNL/spack.git >>/tmp/userdata.log 2>&1
 echo "***********************************" >>/tmp/userdata.log 2>&1
 sudo /opt/spack/spack/bin/spack help -a >>/tmp/userdata.log 2>&1
+echo "***********************************" >>/tmp/userdata.log 2>&1
+sudo /opt/spack/spack/bin/spack install vim  >>/tmp/userdata.log 2>&1
+echo "***********************************" >>/tmp/userdata.log 2>&1
 echo "***********************************" >>/tmp/userdata.log 2>&1
 date >>/tmp/userdata.log 2>&1
 echo ...userdata.sh endinging... >>/tmp/userdata.log 2>&1
