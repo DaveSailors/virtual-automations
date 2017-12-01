d80f3d09d3b05b7851f0f177bdf52059c0f61d2d
Dave Sailors <davesailors@yahoo.com>
2017-11-30 18:15:29 -0800

    adding choco install

---
+++ terraform/windows/deployWinChoco/deployWinChoco.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "us-west-1"
+}
+
+
+resource "aws_instance" "Choco" {
+  ami = "${var.ami_id}"
+  instance_type = "t2.micro"
+  key_name = "HomeWindows"
+  security_groups = [
+    "launch-wizard-1",
+    "SG-Home-Open"
+    ]
+
+
+  user_data = "${file("deployWinChoco/userdata.bat")}"
+}
+
