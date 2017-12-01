401d67e10e707afd9b4a1bd7656bf537b7a6a83d
Dave Sailors <davesailors@yahoo.com>
2017-11-30 18:34:57 -0800

    updates

--- terraform/windows/deployWinChoco/userdata.bat
+++ terraform/windows/deployWinChoco/userdata.bat
@@ -1,8 +1,5 @@
 <script>
 cd  \
-mkdir userdata
-cd userdata
-echo "installing choco" >userdata.log.txt
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
