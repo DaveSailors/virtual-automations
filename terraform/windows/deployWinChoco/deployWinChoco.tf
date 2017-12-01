2b5b0077ea2d976420dbb17d1cd691a375519185
Dave Sailors <davesailors@yahoo.com>
2017-11-30 18:40:16 -0800

    updates

--- terraform/windows/deployWinChoco/deployWinChoco.tf
+++ terraform/windows/deployWinChoco/deployWinChoco.tf
@@ -15,6 +15,6 @@ resource "aws_instance" "Choco" {
     ]
 
 
-  user_data = "${file("deployWinChoco/userdata.bat")}"
+  user_data = "${file("virtual-automations/terraform/windows/deployWinChoco/userdata.bat")}"
 }
 
