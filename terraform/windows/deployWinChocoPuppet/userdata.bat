dc413e984ad8fdb8a1aaa6551bb5832191851fc7
Dave Sailors <davesailors@yahoo.com>
2017-12-02 19:13:41 -0800

    readme stuff

--- terraform/windows/deployWinChocoPuppet/userdata.bat
+++ terraform/windows/deployWinChocoPuppet/userdata.bat
@@ -4,12 +4,16 @@ cd  \
 
 mkdir userdatalog
 
-date /t >>userdatalog\logfile.txt
+date /t >>\userdatalog\logfile.txt
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-choco install -y puppet-agent
+date /t >>\userdatalog\logfile.txt
+choco install -y puppet-agent >>userdatalog\logfile.txt
 
-echo  date /t >>userdatalog\logfile.txt
+date /t >>\userdatalog\logfile.txt
+puppet module install puppetlabs-chocolatey --version 3.0.0 
+
+date /t >>\userdatalog\logfile.txt
 
 </script>
