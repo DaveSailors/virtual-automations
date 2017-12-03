66898d1542d6637c0cc8d102e12298f0fd2ae666
Dave Sailors <davesailors@yahoo.com>
2017-12-03 00:11:27 -0800

    readme stuff

--- terraform/windows/deployWinChocoPuppet/userdata.bat
+++ terraform/windows/deployWinChocoPuppet/userdata.bat
@@ -4,24 +4,23 @@ cd  \
 
 mkdir userdatalog
 
+echo  >>\userdatalog\logfile.txt
+echo --- installing choco >>\userdatalog\logfile.txt
 date /t >>\userdatalog\logfile.txt
+echo  >>\userdatalog\logfile.txt
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
+echo  >>\userdatalog\logfile.txt
+echo --- installing puppet agent >>\userdatalog\logfile.txt
 date /t >>\userdatalog\logfile.txt
+echo  >>\userdatalog\logfile.txt
 choco install -y puppet-agent >>\userdatalog\logfile.txt
 
-setx path "%path%;C:\Program Files\Puppet Labs\Puppet\bin"
-
-echo >>\userdatalog\logfile.txt
-date /t >>\userdatalog\logfile.txt
-C:\Program Files\Puppet Labs\Puppet\bin\puppet help  >>\userdatalog\logfile.txt
-
 echo  >>\userdatalog\logfile.txt
-echo  "installing puppet module puppetlabs-chocolatey" >>\userdatalog\logfile.txt
+echo --- ending script >>\userdatalog\logfile.txt
+echo  >>\userdatalog\logfile.txt
 date /t >>\userdatalog\logfile.txt
-puppet module install puppetlabs-chocolatey --version 3.0.0 >>\userdatalog\logfile.txt 2>&1
-
-date /t >>\userdatalog\logfile.txt
+echo  >>\userdatalog\logfile.txt
 
 </script>
